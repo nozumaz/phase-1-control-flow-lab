a92beed7f8377f1e7f0ac13a211e5cc4573a5fe8
PK <nozumaz@gmail.com>
2023-03-17 08:26:16 -0400

    replies to passengers

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numFeet){
   // Write your code here!
+  if (numFeet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (numFeet > 400 && numFeet < 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (numFeet > 2000 && numFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (numFeet > 2500)
+    return 'No can do.';
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else if (city !== 'NYC') {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip == 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
